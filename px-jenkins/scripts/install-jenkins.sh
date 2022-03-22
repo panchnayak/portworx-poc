deb93301ad12d0646b95af3159617bbe27a32d0a
panchnayak <panchaleswar@outlook.com>
2022-03-22 17:44:30 -0400

    updated

--- px-jenkins/scripts/install-jenkins.sh
+++ px-jenkins/scripts/install-jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-sudo yum install java-11-openjdk-devel -y
-sudo yum install daemonize jenkins -y
+#sudo yum install java-11-openjdk-devel -y
+#sudo yum install daemonize jenkins -y
 J_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
 sudo ln -s $J_HOME /usr/lib/jvm/openjdk11
 export JAVA_HOME=/usr/lib/jvm/openjdk11
