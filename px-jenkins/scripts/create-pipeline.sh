f978820dcd1bddfbbdb96127e6097fd4f0ffabbe
panchnayak <panchaleswar@outlook.com>
2022-03-22 15:30:02 -0400

    updated

--- px-jenkins/scripts/create-pipeline.sh
+++ px-jenkins/scripts/create-pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
 
-java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job eks-px-pipeline < ./scripts/eks-px-pipeline.xml
-java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job gke-px-pipeline < ./scripts/gke-px-pipeline.xml
-java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job argocd-server-tasks < ./scripts/argocd-pipeline.xml
-java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job deploy-cassandra-db < ./scripts/cassandra-db-pipeline.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job eks-px-pipeline < ./px-jenkins/pipelines/eks-px-pipeline.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job gke-px-pipeline < ./px-jenkins/pipelines/gke-px-pipeline.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job argocd-server-tasks < ./px-jenkins/pipelines/argocd-pipeline.xml
+java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job deploy-cassandra-db < ./px-jenkins/pipelines/cassandra-db-pipeline.xml
 
 #sudo systemctl restart jenkins
 
