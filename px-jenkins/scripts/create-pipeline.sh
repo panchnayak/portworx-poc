ad49d00101089a97c654694ab67d2ea9ed2ff789
panchnayak <panchaleswar@outlook.com>
2022-03-22 19:42:01 -0400

    updated

--- px-jenkins/scripts/create-pipeline.sh
+++ px-jenkins/scripts/create-pipeline.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -x
 
-declare -a PipelineList=(
-    "eks-px-pipeline"
-    "gke-px-pipeline"
-    "argocd-server-tasks"
-    "deploy-cassandra-db"
-)
 
-declare -a PathList=(
-    "eks-px-pipeline.xml"
-    "gke-px-pipeline.xml"
-    "argocd-pipeline.xml"
-    "cassandra-db-pipeline.xml"
-)
+declare -a PipelineList=("eks-px-pipeline" "gke-px-pipeline" "argocd-server-tasks" "deploy-cassandra-db")
+declare -a PathList=("eks-px-pipeline.xml" "gke-px-pipeline.xml" "argocd-pipeline.xml" "cassandra-db-pipeline.xml")
 
-for ((i=0;i<=${#PipelineList[@]};i++)); do
+for ((i=0;i<${#PipelineList[@]};i++)); do
    java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job ${PipelineList[i]} < /tmp/pipelines/${PathList[i]}
 done
 
