83e435429cd5faa84cf35bfcc42e879f3e2ca4b9
panchnayak <panchaleswar@outlook.com>
2022-03-24 15:15:33 -0400

    updated

--- px-jenkins/scripts/install-tools.sh
+++ px-jenkins/scripts/install-tools.sh
@@ -3,7 +3,7 @@ sudo yum update -y
 sudo curl -Lo /etc/yum.repos.d/go-repo.repos https://mirror.go-repo.io/centos/go-repo.repo
 echo W2F6dXJlLWNsaV0KbmFtZT1BenVyZSBDTEkKYmFzZXVybD1odHRwczovL3BhY2thZ2VzLm1pY3Jvc29mdC5jb20veXVtcmVwb3MvYXp1cmUtY2xpCmVuYWJsZWQ9MQpncGdjaGVjaz0xCmdwZ2tleT1odHRwczovL3BhY2thZ2VzLm1pY3Jvc29mdC5jb20va2V5cy9taWNyb3NvZnQuYXNjCg== | base64 -d > azure-cli.repo
 sudo cp azure-cli.repo /etc/yum.repos.d/azure-cli.repo
-sudo yum install -y gcc make wget unzip which less openssh-clients python3-pip golang azure-cli epel-release openssl jq
+sudo yum install -y gcc make wget unzip which less openssh-clients python3-pip golang azure-cli epel-release openssl jq etcd
 curl https://sdk.cloud.google.com > install.sh
 sudo bash ./install.sh --disable-prompts --install-dir=/usr/local/
 sudo ln -s /usr/local/google-cloud-sdk/bin/gcloud /usr/local/bin/gcloud
