f799f315b67600b15c0acb3e823b49f39aca91bf
panchnayak <panchaleswar@outlook.com>
2022-03-24 11:36:51 -0400

    updated

--- px-jenkins/scripts/create-pipelines.sh
+++ px-jenkins/scripts/create-pipelines.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -x
 
 
-declare -a PipelineList=("1-Execute-kubectl-commands-for-EKS" "2-EKS-K8s-Cluster-Pipeline"  "3-Deploy-Metro-Cluster" "4-GKE-Cluster-Pipeline"  "5-Deploy-Cassandra-DB" )
-declare -a PathList=("kubectl-command.xml" "eks-px.xml"  "metro-cluster.yaml" "gke-px.xml"  "cassandra-db.xml" )
+declare -a PipelineList=(
+   "1-Execute-kubectl-commands-for-EKS" 
+   "2-EKS-K8s-Cluster-Pipeline"  
+   "3-GKE-Cluster-Pipeline"  
+   "4-Deploy-Metro-Cluster"  
+   "5-Deploy-Cassandra-DB" 
+   "6-Portworx-Backup-Demo" 
+   )
+
+declare -a PathList=(
+   "kubectl-command.xml" 
+   "eks-px.xml"  
+   "gke-px.xml"  
+   "metro-cluster.yaml" 
+   "cassandra-db.xml"  
+   "backup-demo.xml"
+   )
 
 for ((i=0;i<${#PipelineList[@]};i++)); do
    java -jar jenkins-cli.jar -auth $1:$2 -s http://localhost:8080  create-job ${PipelineList[i]} < /tmp/pipelines/${PathList[i]}
