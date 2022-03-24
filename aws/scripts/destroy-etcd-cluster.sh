10d094bec885359cb55f927fad5a2e342570e965
panchnayak <panchaleswar@outlook.com>
2022-03-24 15:20:52 -0400

    updated

--- aws/scripts/destroy-etcd-cluster.sh
+++ aws/scripts/destroy-etcd-cluster.sh
@@ -1,4 +1,1 @@
-aws ec2 run-instances --image-id ami-0c02fb55956c7d316 --count 1 --instance-type t3.small \
-    --key-name jenkins-keypair --security-group-ids $1 --subnet-id $2 \
-    --tags Key=Name,Value=pnayak-singlenode-etcd \
-    --user-data file://aws/scripts/etcd-install.txt
+aaws ec2 terminate-instances
