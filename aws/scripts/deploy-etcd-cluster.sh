d74c5bf5764d592773ceaf6ee8031a785d9171ec
panchnayak <panchaleswar@outlook.com>
2022-03-26 00:32:15 -0400

    updated

--- aws/scripts/deploy-etcd-cluster.sh
+++ aws/scripts/deploy-etcd-cluster.sh
@@ -3,5 +3,5 @@
 aws ec2 run-instances --image-id ami-0c02fb55956c7d316 --count 1 --instance-type t3.small \
     --key-name jenkins-keypair --security-group-ids $1 --subnet-id $2 --associate-public-ip-address \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pnayak-singlenode-etcd}]' \
-    --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 10 } } ]"
+    --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 10 } } ]" \
     --user-data file://aws/scripts/etcd-install.txt
