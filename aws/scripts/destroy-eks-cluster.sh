87d14de68e256429a0d209456321fc2177c092d4
panchnayak <panchaleswar@outlook.com>
2022-04-01 14:22:28 -0400

    updated

--- aws/scripts/destroy-eks-cluster.sh
+++ aws/scripts/destroy-eks-cluster.sh
@@ -9,26 +9,24 @@ fi
 echo "Proceeding to destroy EKS Cluster : $1"
 EKS_CLUSTER_NAME=$1
 NODE_TYPE=t3.large
-AWS_REGION=$2
+FIRST_AWS_REGION=$2
+NUMBER_OF_NODES=3
 
 
-VPC_ID=$(eksctl utils describe-stacks --region $AWS_REGION --cluster $EKS_CLUSTER_NAME | grep vpc- | cut -f 2 -d \")
-PROFILE_ARN=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --region $AWS_REGION --query Reservations[0].Instances[0].IamInstanceProfile.Arn --output text | cut -f 2 -d /)
-ROLE_NAME=$(aws iam get-instance-profile --instance-profile-name $PROFILE_ARN --region $AWS_REGION --query InstanceProfile.Roles[0].RoleName --output text)
-INSTANCES=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --region $AWS_REGION --query Reservations[].Instances[].InstanceId --output text)
+VPC_ID=$(eksctl utils describe-stacks --region $FIRST_AWS_REGION --cluster $EKS_CLUSTER_NAME | grep vpc- | cut -f 2 -d \")
+PROFILE_ARN=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --region $FIRST_AWS_REGION --query Reservations[0].Instances[0].IamInstanceProfile.Arn --output text | cut -f 2 -d /)
+ROLE_NAME=$(aws iam get-instance-profile --instance-profile-name $PROFILE_ARN --region $FIRST_AWS_REGION --query InstanceProfile.Roles[0].RoleName --output text)
+INSTANCES=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --region $FIRST_AWS_REGION --query Reservations[].Instances[].InstanceId --output text)
 
 VOLUMES=$(for j in $INSTANCES; do aws ec2 describe-volumes --region $FIRST_AWS_REGION --filters "Name=attachment.instance-id,Values=$j" "Name=tag:PWX_CLUSTER_ID,Values=px-cluster*" --query Volumes[].Attachments[].VolumeId --output text; done)
 
-aws iam delete-role-policy --role-name $ROLE_NAME --policy-name px-eks-policy --region $AWS_REGION
-
+aws iam delete-role-policy --role-name $ROLE_NAME --policy-name px-eks-policy --region $FIRST_AWS_REGION
 echo "Destroying EKS, in VPC name: $VPC_ID wait about 5 minutes (per cluster)"
-
-eksctl delete cluster --region $AWS_REGION --name $EKS_CLUSTER_NAME --wait >&/tmp/delete
-
+eksctl delete cluster --region $FIRST_AWS_REGION --name $EKS_CLUSTER_NAME --wait >&/tmp/delete
 echo "eks cluster deleted"
 
 for j in $VOLUMES; do
-  aws ec2 delete-volume --region $AWS_REGION --volume-id $j
+  aws ec2 delete-volume --region $FIRST_AWS_REGION --volume-id $j
 done
 
 echo "Attached volumes deleted"
