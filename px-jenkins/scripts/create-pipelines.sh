c9d4c454bcdf613990ec6f8eb5e9e059cbd4ca50
panchnayak <panchaleswar@outlook.com>
2022-04-05 19:33:03 -0400

    updated pipeline

--- px-jenkins/scripts/create-pipelines.sh
+++ px-jenkins/scripts/create-pipelines.sh
@@ -3,13 +3,14 @@
 
 declare -a PipelineList=(
    "1-Execute-kubectl-commands-for-K8s" 
-   "2-Deploy-Independent-AWS-EKS"  
-   "3-Deploy-Independent-GCP-GKE"
-   "4-Deploy-ETCD-Cluster"  
-   "5-Install-Portworx-on-MultiCloud-Env"
-   "6-Complete-Metro-Cluster-Demo"  
-   "7-Deploy-Cassandra-DB-On-Existing-K8s" 
+   "2-Deploy-Independent-AWS-EKS-Cluster"  
+   "3-Deploy-Independent-GCP-GKE-Cluster"
+   "4-Deploy-ETCD-Single-Node-Cluster"  
+   "5-Deploy-Portworx-on-the-Kubernetes-Cluster"
+   "6-Deploy-Cassandra-DB-On-Existing-K8s" 
+   "7-Complete-Metro-DR-Cluster-Demo"  
    "8-Portworx-Backup-Demo" 
+   "9-Install-Portworx-on-MultiCloud-Env"
    )
 
 declare -a PathList=(
@@ -17,10 +18,11 @@ declare -a PathList=(
    "eks-px.xml"  
    "gke-px.xml"
    "etcd-cluster.xml"  
-   "multicloud-cluster.xml"
-   "metro-cluster.xml" 
-   "cassandra-db.xml"  
+   "portworx-install.xml"
+   "cassandra-db.xml" 
+   "metro-cluster.xml"
    "backup-demo.xml"
+    "multicloud-cluster.xml"
    )
 
 for ((i=0;i<${#PipelineList[@]};i++)); do
