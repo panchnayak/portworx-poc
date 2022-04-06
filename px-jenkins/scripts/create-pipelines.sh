100bcde2ff8b73d2f9d0a8004c91fcbd85d5d479
panchnayak <panchaleswar@outlook.com>
2022-04-05 23:10:55 -0400

    kubeconfig updated

--- px-jenkins/scripts/create-pipelines.sh
+++ px-jenkins/scripts/create-pipelines.sh
@@ -4,18 +4,20 @@
 declare -a PipelineList=(
    "1-Deploy-Independent-AWS-EKS-Cluster"  
    "2-Deploy-Independent-GCP-GKE-Cluster"
-   "3-Execute-kubectl-commands-for-K8s" 
-   "4-Deploy-ETCD-Single-Node-Cluster"  
-   "5-Deploy-Portworx-on-the-Kubernetes-Cluster"
-   "6-Deploy-Cassandra-DB-On-Existing-K8s" 
-   "7-Complete-Metro-DR-Cluster-Demo"  
-   "8-Portworx-Backup-Demo" 
-   "9-Install-Portworx-on-MultiCloud-Env"
+   "3-Get-Kubeconfig-file-for-K8S-Cluster"
+   "4-Execute-kubectl-commands-for-K8s" 
+   "5-Deploy-ETCD-Single-Node-Cluster"  
+   "6-Deploy-Portworx-on-the-Kubernetes-Cluster"
+   "7-Deploy-Cassandra-DB-On-Existing-K8s" 
+   "8-Complete-Metro-DR-Cluster-Demo"  
+   "9-Portworx-Backup-Demo" 
+   "10-Install-Portworx-on-MultiCloud-Env"
    )
 
 declare -a PathList=(
    "eks-px.xml"  
    "gke-px.xml"
+   "get-kubeconfig.xml"
    "kubectl-command.xml"
    "etcd-cluster.xml"  
    "portworx-install.xml"
