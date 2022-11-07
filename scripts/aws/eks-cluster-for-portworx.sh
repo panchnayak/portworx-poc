4cfd74b43117d24c4d712b3caefee320d46bd2c9
panchnayak <panchaleswar@outlook.com>
2022-11-07 09:56:54 -0500

    updated

---
+++ scripts/aws/eks-cluster-for-portworx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+if [ $# -lt 3 ]
+  then
+    echo "Error: No Cluster name supplied please supply the Cluster name as the first argument,and NodeGroup name as 2n argument and Region name in the third argument, Not going ahead"
+    exit 0
+fi
+
+
+EKS_CLUSTER_NAME=$1
+NODE_GROUP_NAME=$2
+NODE_TYPE=t3.large
+AWS_REGION=$3
+echo "Proceeding to crate EKS Cluster : $1 in Region $3"
+
+eksctl create cluster -f eks-demo-cluster-east.yaml
+
+echo "EKS Cluster $1 Created in Region $2"
