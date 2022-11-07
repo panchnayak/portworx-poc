84506c498d0aaa28049b0e3b9c7cdccefda01544
panchnayak <panchaleswar@outlook.com>
2022-11-07 09:25:44 -0500

    eks cluster creation updated

--- scripts/aws/create-eks-cluster.sh
+++ scripts/aws/create-eks-cluster.sh
@@ -6,66 +6,13 @@ if [ $# -lt 3 ]
     exit 0
 fi
 
-echo "Proceeding to crate EKS Cluster : $1 in Region $3"
+
 EKS_CLUSTER_NAME=$1
 NODE_GROUP_NAME=$2
 NODE_TYPE=t3.large
 AWS_REGION=$3
-#SECOND_AWS_REGION=$3
-NUMBER_OF_NODES=3
-eksctl create cluster --region $AWS_REGION --nodes $NUMBER_OF_NODES --node-type $NODE_TYPE --name $EKS_CLUSTER_NAME  --nodegroup-name $NODE_GROUP_NAME
-VPC_ID=$(eksctl utils describe-stacks --region $AWS_REGION --cluster $EKS_CLUSTER_NAME | grep vpc- | cut -f 2 -d \")
-PROFILE_ARN=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --region $AWS_REGION --query Reservations[0].Instances[0].IamInstanceProfile.Arn --output text | cut -f 2 -d /)
-ROLE_NAME=$(aws iam get-instance-profile --instance-profile-name $PROFILE_ARN --region $AWS_REGION --query InstanceProfile.Roles[0].RoleName --output text)
-cat <<EOF >/tmp/role.json
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "autoscaling:DescribeAutoScalingGroups",
-                "ec2:AttachVolume",
-                "ec2:DetachVolume",
-                "ec2:CreateTags",
-                "ec2:CreateVolume",
-                "ec2:DeleteTags",
-                "ec2:DeleteVolume",
-                "ec2:DescribeTags",
-                "ec2:DescribeVolumeAttribute",
-                "ec2:DescribeVolumesModifications",
-                "ec2:DescribeVolumeStatus",
-                "ec2:DescribeVolumes",
-                "ec2:DescribeInstances",
-                "ec2:DeleteSnapshot",
-                "ec2:DescribeInstances",
-                "ec2:CreateTags",
-                "ec2:CreateSnapshots",
-                "ec2:DescribeVolumes",
-                "ec2:CreateSnapshot",
-                "ec2:DescribeRegions",
-                "ec2:DescribeSnapshots",
-                "ec2:CreateVolume",
-                "s3:ListBucketMultipartUploads",
-                "s3:ListBucketVersions",
-                "s3:ListBucket",
-                "s3:GetBucketAcl",
-                "s3:ListMultipartUploadParts",
-                "s3:PutObject",
-                "s3:GetObjectAcl",
-                "s3:GetObject",
-                "s3:ListAllMyBuckets",
-                "s3:GetObjectVersionAcl",
-                "s3:DeleteObject",
-                "s3:PutObjectAcl"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
-}
-EOF
-aws iam put-role-policy --role-name $ROLE_NAME --policy-name px-eks-policy --policy-document file:///tmp/role.json --region $AWS_REGION
+echo "Proceeding to crate EKS Cluster : $1 in Region $3"
+
+eksctl create cluster -f eks-demo-cluster-east.yaml
 
 echo "EKS Cluster $1 Created in Region $2"
--- scripts/jenkins/create-pipelines.sh
+++ scripts/jenkins/create-pipelines.sh
@@ -8,29 +8,35 @@ fi
 
 
 declare -a PipelineList=(
-   "1-Deploy-or-Destroy-AWS-EKS-Cluster"  
-   "2-Deploy-or-Destroy-GCP-GKE-Cluster"
-   "3-Get-Kubeconfig-file-for-K8S-Cluster"
-   "4-Execute-kubectl-commands-for-K8s" 
-   "5-Deploy-ETCD-Single-Node-Cluster"  
-   "6-Deploy-or-Destroy-Portworx-on-the-Kubernetes-Cluster"
-   "7-Deploy-or-Destroy-Cassandra-DB-On-Existing-K8s" 
-   "8-Complete-or-Destroy-Metro-DR-Cluster-Demo"  
-   "9-Deploy-or-Destroy-Px-Backup-Demo" 
-   "10-Deploy-or-Destroy-Portworx-on-MultiCloud-Env"
+   "Day-1-Deploy-or-Destroy-AWS-EKS-Cluster"
+   "Day-1-Deploy-or-Destroy-GCP-GKE-Cluster"
+   "Day-1-Get-Kubeconfig-file-for-K8S-Cluster"
+   "Day-1-Execute-kubectl-commands-for-K8s" 
+   "Day-1-Deploy-or-Destroy-Portworx-on-the-Kubernetes-Cluster"
+   "Day-1-Deploy-ETCD-Single-Node-Cluster"
+   "Day-1-Deploy-or-Destroy-Portworx-on-MultiCloud-Env"
+   "Day-1-Complete-or-Destroy-Metro-DR-Cluster-Demo"  
+   "Day-2-Create Petclinic Application"
+   "Day-2-Create=StorageClass"
+   "Day-2-Developr Create Volumes-for-Apps"
+   "Day-2-Deploy-or-Destroy-Cassandra-DB-On-Existing-K8s"
+   "Day-3-Deploy-or-Destroy-Px-Backup-Demo" 
    )
 
 declare -a PathList=(
-   "eks-px.xml"  
+   "eks-px.xml"
    "gke-px.xml"
    "get-kubeconfig.xml"
    "kubectl-command.xml"
-   "etcd-cluster.xml"  
-   "portworx-install.xml"
-   "cassandra-db.xml" 
+   "portworx-install.xml"  
+   "etcd-cluster.xml"
+   "multicloud-cluster.xml"
    "metro-cluster.xml"
+   "cassandra-db.xml"
+   "px-petclinic.xml"
+   "px-storage-class.xml"
+   "px-volume.xml"
    "px-backup-demo.xml"
-    "multicloud-cluster.xml"
    )
 
 for ((i=0;i<${#PipelineList[@]};i++)); do
