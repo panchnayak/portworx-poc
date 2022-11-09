fac840a32a173bb69bfc51fce50f2cb34301c3d2
panchnayak <panchaleswar@outlook.com>
2022-11-08 19:13:03 -0500

    eks updated

--- scripts/aws/create-eks-cluster.sh
+++ scripts/aws/create-eks-cluster.sh
@@ -13,6 +13,41 @@ NODE_TYPE=t3.large
 AWS_REGION=$3
 echo "Proceeding to crate EKS Cluster : $1 in Region $3"
 
-eksctl create cluster -f ./scripts/aws/eks-demo-cluster-east.yaml
+eksctl create cluster -f - <<EOF
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+metadata:
+  name: $EKS_CLUSTER_NAME
+  region: $AWS_REGION
+  version: "1.23"
+managedNodeGroups:
+  - name: $NODE_GROUP_NAME
+    instanceType: t3.large
+    minSize: 3
+    maxSize: 3
+    volumeSize: 20
+    #ami: auto
+    amiFamily: AmazonLinux2
+    labels: {role: worker, "portworx.io/node-type": "storage"}
+    tags:
+      nodegroup-role: worker
+    iam:
+      attachPolicyARNs:
+        - arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy
+        - arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy
+        - arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
+        - arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess
+        - arn:aws:iam::803113055342:policy/rancher-portworx-policy
+        - arn:aws:iam::803113055342:policy/pn-portworx
+      withAddonPolicies:
+        imageBuilder: true
+        autoScaler: true
+        ebs: true
+        fsx: true
+        efs: true
+        albIngress: true
+        cloudWatch: true
+availabilityZones: [ 'us-east-1a', 'us-east-1b', 'us-east-1c' ]
+EOF
 
 echo "EKS Cluster $1 Created in Region $2"
